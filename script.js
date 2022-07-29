ee7e2d5c682a70bde65b0925a4076fc9d62642af
Piotr <websites@hello-24.com>
2022-07-29 14:03:00 -0500

    Creating basic files of application

---
+++ script.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const ourForm = document.getElementById('ourForm');
+const ourField = document.getElementById('ourField');
+
+ourForm.addEventListener('submit', e => {
+  e.preventDefault();
+});
